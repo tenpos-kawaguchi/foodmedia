6beb9cea228595142b4a18bc097cda8cebe3134f
tenpos-kawaguchi <y-kawaguchi@tenpos.com>
2025-04-14 18:58:42 +0900

    Geistのエラー修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/header";
 import Footer from "@/app/footer";
